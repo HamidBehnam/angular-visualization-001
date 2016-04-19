80b7f905b57564babac2352d16637e5ed1c93dd7
Hamid Behnam <hamid.behnam@gmail.com>
2016-04-19 11:30:27 -0700

    inner range close button

--- src/js/individuals/modules.content/av.directives/avCloseInnerRange.js
+++ src/js/individuals/modules.content/av.directives/avCloseInnerRange.js
@@ -1,3 +1,19 @@
 /**
  * Created by hamidbehnam on 4/19/16.
  */
+
+angular.module("av.directives")
+    .directive("avCloseInnerRange", avCloseInnerRange);
+
+function avCloseInnerRange() {
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            $(element).on("click", function (event) {
+                var innerRangeHeight = $(event.target).closest(".inner-range").innerHeight();
+                $(event.target).closest(".inner-range").animate({bottom: "-=".concat(innerRangeHeight, "px")});
+                $(event.target).closest(".inner-range").prev().find("li").each(function (index, element) { $(this).removeClass("active");});
+            });
+        }
+    };
+}
