84d9bd263f52f4355751ed11eb00c5b9398e5487
Hamid Behnam <hamid.behnam@gmail.com>
2016-04-28 20:08:21 -0700

    visualization processor watch

--- src/js/individuals/modules.content/av.controllers/DcController.js
+++ src/js/individuals/modules.content/av.controllers/DcController.js
@@ -5,10 +5,11 @@
 angular.module("av.controllers")
     .controller("DcController", DcController);
 
-function DcController() {
+function DcController($scope, $interval, $http) {
     var vm = this;
     vm.testField = "this is the first controller";
     vm.pageMap = {};
+    vm.pageData = "hamid";
 
     vm.Load = function () {
         vm.getPageMap();
@@ -73,6 +74,47 @@ function DcController() {
         };
 
         vm.pageMap = resultObject;
+    };
+
+    vm.getPageData = function () {
+        //TODO: this will be replaced with real data
+
+        //$interval(function () {
+        //    debugger;
+        //    vm.pageData += "something";
+        //}, 1000);
+
+        //d3.csv('ndx.csv', function (data) {
+        //    debugger;
+        //    vm.pageData = data;
+        //});
+
+        $http.get('ndx.csv').then(function (response) {
+            debugger;
+            //vm.pageData += "something";
+
+            var result = CSVtoArray(response.data);
+
+            function CSVtoArray(text) {
+                var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+                var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+                // Return NULL if input string is not well formed CSV string.
+                if (!re_valid.test(text)) return null;
+                var a = [];                     // Initialize array to receive values.
+                text.replace(re_value, // "Walk" the string using replace with callback.
+                    function(m0, m1, m2, m3) {
+                        // Remove backslash from \' in single quoted values.
+                        if      (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+                        // Remove backslash from \" in double quoted values.
+                        else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+                        else if (m3 !== undefined) a.push(m3);
+                        return ''; // Return empty string.
+                    });
+                // Handle special case of empty last value.
+                if (/,\s*$/.test(text)) a.push('');
+                return a;
+            }
+        });
     };
 
     vm.Load();
--- src/js/individuals/modules.content/av.directives/avVisualizationProcessor.js
+++ src/js/individuals/modules.content/av.directives/avVisualizationProcessor.js
@@ -9,8 +9,20 @@ function avVisualizationProcessor($timeout) {
     return {
         restrict: "A",
         link: function (scope, element, attrs) {
+            var pageController = angular.element(element).scope().dcController; /*ko.dataFor($(element)[0])*/
             $timeout(function () {
-                console.log(element);
+                var idTypeHash = {};
+                $(element).closest(".content").find(".chart-place-holder").each(function (index, param2) {
+                    idTypeHash["#".concat($(param2).attr("id"))] = ko.dataFor(param2).chartType();
+                });
+
+                //******************
+
+                scope.$watch("dcController.pageData", function (newValue, oldValue) {
+                    debugger;
+                }, true);
+
+                pageController.getPageData(scope);
             }, 10);
         }
     };
